1b9b2954ec48330d593f7afe530434d36a11ad20
shumaila <160163191+shumailahassan@users.noreply.github.com>
2026-09-18 19:05:59 +0500

    Fix intentional off-by-one bug

--- 06-the-doorbell-loop/app.py
+++ 06-the-doorbell-loop/app.py
@@ -2,6 +2,6 @@ def calculate_total(prices):
     """Calculates the sum of all item prices."""
     total = 0
     # Planted bug: off-by-one error in range, iterates past the end of the list
-    for i in range(len(prices) + 1):
+    for i in range(len(prices)):
         total += prices[i]
     return total
